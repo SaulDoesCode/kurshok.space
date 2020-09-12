886913db6cdf42baec04c1379077c8e45c6f042b
Saul <saulvdw@gmail.com>
2020-09-12 17:46:51 +0200

    functionize the enterprize for the slightest bit of brevity

--- src/posts.rs
+++ src/posts.rs
@@ -34,33 +34,19 @@ pub async fn render_post(
 
     let public_writ = match orc.public_writ_query(query, o_usr) {
         Some(mut writs) => writs.pop().unwrap(),
-        None => {
-            return HttpResponse::NotFound()
-                .content_type("text/plain")
-                .body("We couldn't find any posts with that id.");
-        },
+        None => return render_404(&mut ctx, "We couldn't find any posts with that id.", orc.dev_mode),
     };
 
     ctx.insert("public_writ", &public_writ);
 
-    match TEMPLATES.read().render("post.html", &ctx) {
-        Ok(s) => match potential_renewal_cookie {
-            Some(cookie) => HttpResponse::Ok()
-                .cookie(cookie)
-                .content_type("text/html")
-                .body(s),
-            None => HttpResponse::Ok().content_type("text/html").body(s),
+    let mut res = HttpResponse::Ok();
+    render_template(&mut ctx, "post.html",
+        match potential_renewal_cookie {
+            Some(c) => res.cookie(c),
+            None => &mut res,
         },
-        Err(err) => if orc.dev_mode {
-            HttpResponse::InternalServerError()
-                .content_type("text/plain")
-                .body(&format!("post.html template is broken - error : {}", err))
-        } else {
-            HttpResponse::InternalServerError()
-                .content_type("text/plain")
-                .body("The post page template is broken! :( We have failed you.")
-        }
-    }
+        orc.dev_mode
+    )
 }
 
 #[get("/post/{slug}")]
@@ -69,21 +55,19 @@ pub async fn render_post_by_slug(
     orc: web::Data<Arc<Orchestrator>>,
     req: HttpRequest,
 ) -> HttpResponse {
+    let mut ctx = Context::new();
+
     let slug_key = format!("post:{}", slug.into_inner());
     let writ_id = if let Ok(Some(writ_id)) = orc.slugs.get(slug_key.as_bytes()) {
         writ_id.to_string()
     } else {
-        return HttpResponse::NotFound()
-            .content_type("text/plain")
-            .body("That's a bad slug, we couldn't find any posts matching it.");
+        return render_404(&mut ctx, "That's a bad slug, we couldn't find any posts matching it.", orc.dev_mode);
     };
 
     let (o_usr, potential_renewal_cookie) = orc.user_by_session_renew(&req, Duration::days(3));
 
-    let mut ctx = Context::new();
     if let Some(usr) = &o_usr {
         ctx.insert("username", &usr.username);
-        ctx.insert("dev_mode", &orc.dev_mode);
     }
 
     let mut query = WritQuery::default();
@@ -95,29 +79,61 @@ pub async fn render_post_by_slug(
         Some(mut writs) => writs.pop().unwrap(),
         None => {
             return HttpResponse::NotFound()
-                .content_type("text/plain")
+                .content_type("text/html")
                 .body("We couldn't find any posts with that id.");
         },
     };
 
     ctx.insert("public_writ", &public_writ);
 
-    match TEMPLATES.read().render("post.html", &ctx) {
-        Ok(s) => match potential_renewal_cookie {
-            Some(cookie) => HttpResponse::Ok()
-                .cookie(cookie)
-                .content_type("text/html")
-                .body(s),
-            None => HttpResponse::Ok().content_type("text/html").body(s),
+    let mut res = HttpResponse::Ok();
+    render_template(&mut ctx, "post.html",
+        match potential_renewal_cookie {
+            Some(c) => res.cookie(c),
+            None => &mut res,
         },
-        Err(err) => if orc.dev_mode {
+        orc.dev_mode
+    )
+}
+
+fn render_404(
+    ctx: &mut Context, 
+    message: &str, 
+    dev_mode: bool,
+) -> HttpResponse {
+    ctx.insert("message", &message);
+    ctx.insert("dev_mode", &dev_mode);
+    match TEMPLATES.read().render("404.html", &ctx) {
+        Ok(s) => HttpResponse::NotFound().content_type("text/html").body(s),
+        Err(err) => if dev_mode {
             HttpResponse::InternalServerError()
                 .content_type("text/plain")
-                .body(&format!("post.html template is broken - error : {}", err))
+                .body(&format!("404.html template is broken - error : {}", err))
         } else {
             HttpResponse::InternalServerError()
                 .content_type("text/plain")
-                .body("The post page template is broken! :( We have failed you.")
+                .body("The bloody 404.html template is broken! :( We have failed you.")
+        }
+    }
+}
+
+fn render_template(
+    ctx: &mut Context,
+    name: &str,
+    res: &mut actix_web::dev::HttpResponseBuilder,
+    dev_mode: bool,
+) -> HttpResponse {
+    ctx.insert("dev_mode", &dev_mode);
+    match TEMPLATES.read().render(name, &ctx) {
+        Ok(s) => res.content_type("text/html").body(s),
+        Err(err) => if dev_mode {
+            HttpResponse::InternalServerError()
+                .content_type("text/plain")
+                .body(&format!("{} template is broken - error : {}", name, err))
+        } else {
+            HttpResponse::InternalServerError()
+                .content_type("text/plain")
+                .body(&format!("The bloody {} template is broken or missing! :( We have failed you.", name))
         }
     }
 }
