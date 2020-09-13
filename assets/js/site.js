e6874e1a9c0c3d20e86245280cd6e4c79b6c1c8f
Saul <saulvdw@gmail.com>
2020-09-13 03:56:52 +0200

    +generic view toggler thingy

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,7 @@
 import domlib from '/js/domlib.min.js'
 
 const app = domlib.emitter()
-app.d = domlib
+const d = app.d = domlib, df = domlib.domfn
 const jsonHTTPMethod = method => (url, body) => fetch(url, {
     method,
     headers: {'Content-Type': 'application/json'},
@@ -16,7 +16,58 @@ app.writQuery = async (query = {}) => {
     const res = await app.jsonPost('/writs', query)
     return await res.json()
 }
+app.editableWritQuery = async (query = {}) => {
+    if (isNaN(query.page)) query.page = 1
+    if (!query.kind) query.kind = 'post'
+    const res = await app.jsonPost('/editable-writs', query)
+    return await res.json()
+}
+
+app.setupToggleSituation = (launcher, view, renderTo = 'body') => {
+    const ts = {}
+
+    ts.clickOutHandler = d.on.pointerdown(document.body, e => {
+        if (
+            e.target != view &&
+            !view.contains(e.target) &&
+            df.hasClass(view, 'open') &&
+            e.target != launcher
+        ) {
+            e.preventDefault()
+            ts.toggleView(false)
+            ts.clickOutHandler.off()
+        }
+    }).off()
+
+    ts.launchEventHandler = d.on.pointerdown(launcher, e => {
+        e.preventDefault()
+        ts.toggleView()
+    })
+
+    ts.toggleView = (state = !df.hasClass(view, 'open')) => {
+        df.class(view, 'open', state)
+        df.class(launcher, 'active', state)
+        if (state) {
+            d.render(view, renderTo)
+            ts.clickOutHandler.on()
+        } else {
+            df.remove(view)
+        }
+    }
+
+    ts.on = true
+    ts.toggle = (state = !ts.on) => {
+        ts.toggleView(state)
+        if (!state) {
+            ts.launchEventHandler.off()
+            ts.clickOutHandler.off()
+            df.remove(view)
+        }
+        return ts.on = state
+    }
+
+    return ts
+}
 
 window.app = app
-
 export default app
