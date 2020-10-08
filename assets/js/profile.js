17bf0af151104830264c197429145e0e22b26aef
Saul <saulvdw@gmail.com>
2020-10-08 19:42:23 +0200

    +basic profile view going with working logout btn

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,3 +1,21 @@
 import app from '/js/site.min.js'
 const d = app.d, df = d.domfn
 
+const profileLauncher = d.query('.profile-launcher')
+
+const {
+    profileView,
+    logoutBtn
+} = d.h/* html */`
+<section class="profile-view" ref="profileView">
+    <h4>Profile of ${app.user.username}</h4>
+    <button class="logout-btn" ref="logoutBtn">Log out</button>
+</section>`.collect()
+
+app.profileViewToggle = app.setupToggleSituation(profileLauncher, app.profileView = profileView)
+
+d.once.click(logoutBtn, async e => {
+    const res = await (await fetch('/auth', {method: 'DELETE'})).json()
+    console.log(res)
+    window.location.reload()
+})
