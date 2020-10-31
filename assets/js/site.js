126962d558b6cddad7d1d0507836235800515783
Saul <saulvdw@gmail.com>
2020-10-31 23:53:04 +0200

    implemented toast

--- assets/js/site.js
+++ assets/js/site.js
@@ -168,5 +168,48 @@ app.cv = (event, state) => {
 app.cv.check = event => !!app.cv.map[event]
 app.cv.map = Object.create(null)
 
+app.toastContainer = df.section({
+    $: 'body',
+    class: 'toast-container',
+    contingentVisibility: 'gotToast'
+})
+
+app.cv('gotToast', false)
+
+app.toastList = new Set()
+app.toast = new Proxy((kind, msg, displayTime = 15000) => {
+    app.cv('gotToast', true)
+    const dismiss = () => {
+        df.remove(toast)
+        app.toastList.delete(toast)
+        if (app.toastList.size == 0) app.cv('gotToast', false)
+    }
+    const toast = df.div({
+        $: app.toastContainer,
+        class: 'toast ' + kind
+    },
+        df.span(msg),
+        df.span({
+            class: 'dismiss-btn',
+            onceclick() {
+                console.log('moo')
+                dismiss()
+            }
+        },
+            domlib.html(/*html*/
+`<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" style="width: 16px; height: 22px;">
+    <polygon fill="rgb(255, 66, 66)" points="11.649 9.882 18.262 3.267 16.495 1.5 9.881 8.114 3.267 1.5 1.5 3.267 8.114 9.883 1.5 16.497 3.267 18.264 9.881 11.65 16.495 18.264 18.262 16.497">
+    </polygon>
+</svg>`
+            )
+        )
+    )
+    app.toastList.add(toast)
+    df.remove(toast, displayTime).then(dismiss)
+    return toast
+}, {
+    get: (toast, kind) => toast.bind(null, kind)
+})
+
 window.app = app
 export default app
