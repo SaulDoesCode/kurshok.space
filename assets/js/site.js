28b3ca67a0258dcae07ee2d8d8d940e050e4f4c5
Saul <saulvdw@gmail.com>
2020-12-15 03:55:36 +0200

    use dismissIcon svg image with app.dismissIcon()

--- assets/js/site.js
+++ assets/js/site.js
@@ -160,6 +160,13 @@ domlib.createElementPlugins.contingentVisibility = (event, el) => {
     })
 }
 
+app.dismissIcon = () => domlib.html( /*html*/
+    `<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" style="width: 16px; height: 22px;">
+    <polygon fill="rgb(255, 66, 66)" points="11.649 9.882 18.262 3.267 16.495 1.5 9.881 8.114 3.267 1.5 1.5 3.267 8.114 9.883 1.5 16.497 3.267 18.264 9.881 11.65 16.495 18.264 18.262 16.497">
+    </polygon>
+</svg>`
+)
+
 app.cv = (event, state) => {
     if (app.cv.map[event] == state) return false
     else if (state === undefined) state = !app.cv.map[event]
@@ -196,12 +203,7 @@ app.toast = new Proxy((kind, msg, displayTime = 15000) => {
                 dismiss()
             }
         },
-            domlib.html(/*html*/
-`<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" style="width: 16px; height: 22px;">
-    <polygon fill="rgb(255, 66, 66)" points="11.649 9.882 18.262 3.267 16.495 1.5 9.881 8.114 3.267 1.5 1.5 3.267 8.114 9.883 1.5 16.497 3.267 18.264 9.881 11.65 16.495 18.264 18.262 16.497">
-    </polygon>
-</svg>`
-            )
+            app.dismissIcon()
         )
     )
     app.toastList.add(toast)
