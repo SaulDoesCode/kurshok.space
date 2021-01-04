bbd29b5ca914462203e24a041d88e302c63f5c83
Saul <saulvdw@gmail.com>
2021-01-04 13:47:46 +0200

    use expirable_data system instead of limits

--- src/ratelimiter.rs
+++ src/ratelimiter.rs
@@ -1,54 +1,41 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use sled::{Db, Tree};
-use std::sync::{
-  atomic::{AtomicU64, Ordering::SeqCst},
-  Arc,
-};
 use time::{Duration, OffsetDateTime};
+
+use crate::orchestrator::ORC;
 
 fn now() -> i64 {
   OffsetDateTime::now_utc().unix_timestamp()
 }
 
 pub struct RateLimiter {
-  pub store: Tree,
-  pub limit: u64,
-  pub count: Arc<AtomicU64>,
+  pub store: Tree
 }
 
 impl RateLimiter {
-  pub fn setup_default(db: &Db) -> Self {
+  pub fn setup(db: &Db) -> Self {
     let store = db.open_tree("rl").expect("failed to open sled rl tree for ratelimiter");
-    let count = Arc::new(AtomicU64::new(store.len() as u64));
-    RateLimiter {
-      store,
-      limit: 100_000,
-      count,
-    }
-  }
-
-  pub fn new(store: Tree, limit: u64) -> Self {
-    let count = Arc::new(AtomicU64::new(0));
-    count.fetch_add(store.len() as u64, SeqCst);
-    Self {
-      store,
-      limit,
-      count,
-    }
+    RateLimiter {store}
   }
 
   pub fn hit(&self, data: &[u8], threshhold: u32, dur: Duration) -> Option<RateLimited> {
     if let Some(mut rl) = self.get(data) {
-      if self
-        .store
-        .insert(data, rl.hit(1, threshhold, dur).to_bin())
-        .is_ok()
-      {
+      if self.store.insert(data, rl.hit(1, threshhold, &dur).to_bin()).is_ok() {
+        ORC.expire_key(
+          rl.seconds_left(),
+          "rl".to_string(),
+          data
+        );
         return Some(rl);
       }
     }
     let rl = RateLimited::new();
     if self.insert(data, rl.to_bin()) {
+      ORC.expire_key(
+        rl.seconds_left(),
+        "rl".to_string(),
+        data
+      );
       return Some(rl);
     }
     None
@@ -56,22 +43,16 @@ impl RateLimiter {
 
   pub fn forget(&self, data: &[u8]) -> bool {
     if let Ok(Some(_)) = self.store.remove(data) {
-      self.count.fetch_sub(1, SeqCst);
+      ORC.unexpire_key("rl".to_string(), data);
       return true;
     }
     false
   }
 
-  pub fn insert(&self, data: &[u8], entry: Vec<u8>) -> bool {
-    if self.store.insert(data, entry).is_ok()
-      && self.count.fetch_add(1, SeqCst) == self.limit
-      && self.store.pop_min().is_ok()
-    {
-      self.count.fetch_sub(1, SeqCst);
-      return true;
-    }
-    false
+  fn insert(&self, data: &[u8], entry: Vec<u8>) -> bool {
+    self.store.insert(data, entry).is_ok()
   }
+
   pub fn get(&self, data: &[u8]) -> Option<RateLimited> {
     match self.store.get(data) {
       Ok(rl) => rl.map(|raw| RateLimited::try_from_slice(&raw).unwrap()),
@@ -113,11 +94,15 @@ impl RateLimited {
     self.duration_left().whole_minutes()
   }
 
+  pub fn seconds_left(&self) -> i64 {
+    self.duration_left().whole_seconds()
+  }
+
   pub fn duration_left(&self) -> Duration {
     Duration::seconds(self.timeout - now())
   }
 
-  pub fn hit(&mut self, hits: u32, threshhold: u32, dur: Duration) -> &mut RateLimited {
+  pub fn hit(&mut self, hits: u32, threshhold: u32, dur: &Duration) -> &mut RateLimited {
     let now = now();
     let dur_secs = dur.whole_seconds();
     self.hits += hits;
