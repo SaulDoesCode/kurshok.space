1b7f9731d0f151fc2b8e362a8e46f97519281710
Saul <saulvdw@gmail.com>
2021-01-11 00:21:59 +0200

    beginings of async email sending with status check

--- src/email.rs
+++ src/email.rs
@@ -1,3 +1,4 @@
+use borsh::{BorshDeserialize, BorshSerialize};
 use lettre::{
     transport::smtp::{
         authentication::Credentials
@@ -8,7 +9,9 @@ use lettre::{
 };
 
 use super::CONF;
-use crate::orchestrator::{ORC, Orchestrator};
+use crate::{
+    orchestrator::{ORC, Orchestrator}
+};
 
 // TODO: Send emails in non-blocking tokio tasks
 
@@ -72,6 +75,53 @@ impl Orchestrator {
             }
         });
     }
+
+    pub fn send_email_with_status_identifier(&self, sid: Vec<u8>, email: Message) {
+        tokio::spawn(async move {
+            if let Ok(transport) = SmtpTransport::starttls_relay(&EMAIL_CONF.mail_server) {
+                let mailer = transport.credentials(Credentials::new(
+                    EMAIL_CONF.smtp_username.clone(),
+                    EMAIL_CONF.smtp_password.clone()
+                )).build();
+
+                if let Ok(Some(_)) = ORC.email_statuses.get(&sid) {
+                    return // TODO: handle this with websocket notifications
+                }
+                if let Err(_) = ORC.email_statuses.insert(&sid, EmailStatus::Sending.try_to_vec().unwrap()) {
+                    return
+                }
+                ORC.expire_key(60 * 6, "email_statuses".to_string(), &sid);
+
+                match mailer.send(&email) {
+                    Ok(response) => {
+                        if response.is_positive() {
+                            if let Err(_) = ORC.email_statuses.insert(&sid, EmailStatus::Sent.try_to_vec().unwrap()) {}
+                            ORC.unexpire_key("email_statuses".to_string(), &sid);
+                            ORC.expire_key(60 * 6, "email_statuses".to_string(), &sid);
+                        } else {
+                            let reason = Some(response.message.join("\n"));
+                            if let Err(_) = ORC.email_statuses.insert(&sid, EmailStatus::Failed(reason).try_to_vec().unwrap()) {}
+                            ORC.unexpire_key("email_statuses".to_string(), &sid);
+                            ORC.expire_key(60 * 6, "email_statuses".to_string(), &sid);
+                        }
+                    },
+                    Err(e) => {
+                        if ORC.dev_mode {
+                            println!("Could not send email: {:?}", e);
+                        }
+                        let reason = Some(format!("failed to send email: {}", e));
+                        if let Err(_) = ORC.email_statuses.insert(&sid, EmailStatus::Failed(reason).try_to_vec().unwrap()) {}
+                        ORC.unexpire_key("email_statuses".to_string(), &sid);
+                        ORC.expire_key(60 * 6, "email_statuses".to_string(), &sid);
+                    },
+                }
+            }
+        });
+    }
 }
-
-
+#[derive(BorshSerialize, BorshDeserialize)]
+pub enum EmailStatus{
+    Sending,
+    Sent,
+    Failed(Option<String>)
+}
